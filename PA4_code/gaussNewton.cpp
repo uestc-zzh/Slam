76accc5c78135ef6a344c8d723f9fc89720f5652
uestc-zzh <zhouzihan1110@foxmail.com>
2022-08-30 20:16:11 +0800

    GN data2file; pynb plot result

--- PA4_code/gaussNewton.cpp
+++ PA4_code/gaussNewton.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <Eigen/Core>
 #include <Eigen/Dense>
@@ -15,19 +16,23 @@ int main(int argc, char **argv)
     double inv_sigma = 1.0 / w_sigma;
     cv::RNG rng; // opencv随机数产生器
 
+    ofstream output;
+    output.open("../origin_data.txt");
     vector<double> x_data, y_data; // 数据
     for (int i = 0; i < N; i++)
     {
         double x = i / 100.0;
         x_data.push_back(x);
         y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+        output<<y_data[i]<<endl;
     }
+    output.close();
 
     // 开始Gauss-Newton迭代
     int iterations = 100;
     double cost = 0, lastCost = 0; // 本次迭代的cost和上一次迭代的cost
 
-    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+    // chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
     for (int iter = 0; iter < iterations; iter++)
     {
 
@@ -64,7 +69,7 @@ int main(int argc, char **argv)
             break;
         }
 
-        ae += dx[0];
+        ae += dx[0];    // 将自变量增加相应步长，继续下一轮的迭代
         be += dx[1];
         ce += dx[2];
 
@@ -73,10 +78,18 @@ int main(int argc, char **argv)
         cout << "total cost: " << cost << ", \t\tupdate: " << dx.transpose() << "\t\testimated params: " << ae << "," << be << "," << ce << endl;
     }
 
-    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-    cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
-    cout << "estimated abc = " << ae << ", " << be << ", " << ce << endl;
+    // chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+    // chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    // cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+    // cout << "estimated abc = " << ae << ", " << be << ", " << ce << endl;
+    output.open("../out_data.txt");
+    vector<double>out_data;
+    for(int i=0;i<N;i++){
+        double x=i/100.0;
+        out_data.push_back(exp(ae*x*x+be*x+ce));
+        output<<out_data[i]<<endl;
+    }
+    output.close();
 
     return 0;
 }
